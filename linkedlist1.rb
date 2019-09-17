a106237bc81266af9c67bcdaa11678a9d2781b4f
long-hoang <thoanglong@gmail.com>
2019-09-17 16:36:05 +0000

    Initial Commit

---
+++ linkedlist1.rb
@@ -0,0 +1,94 @@
+# Linked List # 1
+
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+class Stack
+    attr_reader :data
+
+    def initialize
+        @data = nil
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        stacked = LinkedListNode.new(value, data)
+        @data = stacked
+    end
+
+    # Pop an item off the stack.
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        top_item = data.value
+        @data = data.next_node
+        return top_item
+    end
+
+end
+
+
+
+def reverse_list(list)
+    reversed = Stack.new
+    
+    while list
+      moving_item = list.value
+      reversed.push(moving_item)
+      list = list.next_node
+    end
+
+    return reversed.data
+end
+
+# Test Linked List 
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+
+print_values(node3)
+
+puts "-----"
+
+revlist = reverse_list(node3)
+
+print_values(revlist)
+
+=begin
+  
+
+  # Test Stack
+
+test_stack = Stack.new
+test_stack.push(10)
+test_stack.push(11)
+test_stack.push(12)
+test_stack.push(13)
+
+print_values(test_stack.data)
+puts test_stack.pop
+print_values(test_stack.data)
+puts test_stack.pop
+print_values(test_stack.data) 
+=end
+
+
+
